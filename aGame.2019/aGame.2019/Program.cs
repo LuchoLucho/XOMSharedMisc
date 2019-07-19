d254871185b3b28dc54d78c204a054e24c4ecd40
lucho83utn@hotmail.com <lucho83utn@hotmail.com>
2019-07-19 11:34:41 -0300

    Initial commit with all code

---
+++ aGame.2019/aGame.2019/Program.cs
@@ -0,0 +1,114 @@
+﻿using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace aGame._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sampleRate = 180 + 1;//The 1 is because of headers
+            Complex32[] samples = new Complex32[sampleRate];
+            Complex32[] originalSamples = new Complex32[sampleRate];
+            Complex32[] inverseSamples = new Complex32[sampleRate];
+            string[] lines = Properties.Resources.CustomerDataTimeSeries.Split(new char[] { '\r', '\n' },StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<DateTime, float> parsedData = new Dictionary<DateTime, float>();
+            DateTime[] sameDateTimes = new DateTime[sampleRate];
+            //----
+            IOrderedEnumerable<KeyValuePair<DateTime, float>> orderD = getDataSortedByDate(sampleRate, lines, parsedData);
+            int j = 0;
+            foreach (KeyValuePair<DateTime, float> keyValuePair in orderD)
+            {
+                float sum = 0;
+                float valueToPut = keyValuePair.Value + sum;
+                samples[j] = new Complex32(valueToPut, 0);
+                originalSamples[j] = new Complex32(valueToPut, 0);
+                sameDateTimes[j] = keyValuePair.Key;
+                j++;
+            }
+            Fourier.Forward(samples, FourierOptions.Matlab);
+            applyFilter(sampleRate, samples, inverseSamples);
+            Fourier.Inverse(inverseSamples, FourierOptions.Matlab);
+            saveDataAsNewCSV(samples, originalSamples, inverseSamples, sameDateTimes);
+        }
+
+        private static void saveDataAsNewCSV(Complex32[] samples, Complex32[] originalSamples, Complex32[] inverseSamples, DateTime[] sameDateTimes)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < samples.Length; i++)
+            {
+                sb.AppendLine(i + "," + originalSamples[i].Real + "," + samples[i].Magnitude + "," + inverseSamples[i].Real + "," + sameDateTimes[i]);
+            }
+            File.WriteAllText("fft.csv", sb.ToString());
+        }
+
+        private static void applyFilter(int sampleRate, Complex32[] samples, Complex32[] inverseSamples)
+        {
+            for (int i = 0; i < sampleRate; i++)
+            {
+                if ((Math.Abs(i - 26) > 4) && (i > 7))
+                {
+                    inverseSamples[i] = new Complex32(0, 0);
+                }
+                else
+                {
+                    inverseSamples[i] = new Complex32(samples[i].Real, samples[i].Imaginary);
+                }
+            }
+        }
+
+        private static IOrderedEnumerable<KeyValuePair<DateTime, float>> getDataSortedByDate(int sampleRate, string[] lines, Dictionary<DateTime, float> parsedData)
+        {
+            for (int i = 1; i < sampleRate + 1; i++)
+            {
+                string[] items = lines[i].Split(new char[] { ',' , '\t'});
+                string year = items[0];
+                string month = items[1];
+                int numberOfMonth = resolveMonth(month);
+                string day = items[2];
+                DateTime currentDate = new DateTime(int.Parse(year), numberOfMonth, int.Parse(day));
+                string value = items[4];//3: Invoice number;
+                string value2 = items[3];
+                parsedData.Add(currentDate, float.Parse(value));
+            }
+            var orderD = parsedData.OrderBy(x => x.Key);
+            return orderD;
+        }
+
+        private static int resolveMonth(string month)
+        {
+            if (month.StartsWith("Jan", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return 1;
+            }
+            if (month.StartsWith("Feb", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return 2;
+            }
+            if (month.StartsWith("Mar", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return 3;
+            }
+            if (month.StartsWith("Apr", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return 4;
+            }
+            if (month.StartsWith("May", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return 5;
+            }
+            if (month.StartsWith("Jun", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return 6;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ aGame.2019/aGame.2019/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aGame.2019")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("aGame.2019")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e08b34ff-fb56-49a8-b779-c8ea5d69c805")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
